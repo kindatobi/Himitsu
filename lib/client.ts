2cd674a15224b399cc5f89c6815800f3ac967be4
blur <thetobiterra@gmail.com>
2026-03-29 00:20:00 +0100

    bug fix

--- lib/client.ts
+++ lib/client.ts
@@ -2,7 +2,5 @@ import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
 export const api = treaty<App>(
-  process.env.NODE_ENV === "production"
-    ? window.location.origin
-    : "localhost:3000",
+  process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000",
 ).api;

977731910387e02d6842ca786817647cb19c58ba
blur <thetobiterra@gmail.com>
2026-03-29 00:12:23 +0100

    cors bug

--- lib/client.ts
+++ lib/client.ts
@@ -3,6 +3,6 @@ import type { App } from "../app/api/[[...slugs]]/route";
 
 export const api = treaty<App>(
   process.env.NODE_ENV === "production"
-    ? process.env.NEXT_PUBLIC_API_URL!
+    ? window.location.origin
     : "localhost:3000",
 ).api;

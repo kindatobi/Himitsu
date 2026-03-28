b46826ee6d8129218164d07614be45e906ff5589
blur <thetobiterra@gmail.com>
2026-03-28 23:53:53 +0100

    little fix path from local to prod

--- lib/client.ts
+++ lib/client.ts
@@ -1,4 +1,8 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
-export const api = treaty<App>("localhost:3000").api;
+export const api = treaty<App>(
+  process.env.NODE_ENV === "production"
+    ? process.env.NEXT_PUBLIC_API_URL!
+    : "localhost:3000",
+).api;
